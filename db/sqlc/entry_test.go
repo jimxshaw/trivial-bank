235034a97c3046af32f4863229d9bf23647a33ed
Jim Shaw <jimxshaw@gmail.com>
2023-08-19 17:44:14 -0500

    Add TestGetEntry.

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -15,6 +15,32 @@ func TestCreateEntry(t *testing.T) {
 	createRandomEntry(t)
 }
 
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+
+	query := `
+		SELECT id, account_id, amount, created_at 
+		FROM entries
+		WHERE id = $1 LIMIT 1
+	`
+
+	rows := sqlmock.NewRows([]string{"id", "account_id", "amount", "created_at"}).
+		AddRow(entry1.ID, entry1.AccountID, entry1.Amount, entry1.CreatedAt)
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(entry1.ID).
+		WillReturnRows(rows)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
 func createRandomEntry(t *testing.T) Entry {
 	account := createRandomAccount(t)
 
