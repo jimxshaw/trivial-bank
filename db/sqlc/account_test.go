ac89e5b19df200ca366cb35aa01a00e6f1e86c77
Jim Shaw <jimxshaw@gmail.com>
2023-08-19 15:40:07 -0500

    Add TestGetAccount.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -15,6 +15,20 @@ func TestCreateAccount(t *testing.T) {
 	createRandomAccount(t)
 }
 
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
 func createRandomAccount(t *testing.T) Account {
 	params := CreateAccountParams{
 		Owner:    util.RandomOwner(),
