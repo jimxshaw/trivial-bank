cc05bf3ef68c9fe33ded0291c928c6a8c09c9658
Jim Shaw <jimxshaw@gmail.com>
2023-08-19 13:58:30 -0500

    Add util package with random.go.

---
+++ util/random.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+// RandomInt generates a random int between min and max.
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of n characters.
+func RandomString(n int) string {
+	var sb strings.Builder
+	l := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		char := alphabet[rand.Intn(l)]
+		sb.WriteByte(char)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name.
+func RandomOwner() string {
+	return RandomString(8)
+}
+
+// RandomAmount generates a random amount of money.
+func RandomAmount() int64 {
+	return RandomInt(0, 1_000_000)
+}
+
+// RandomCurrency generates a random currency code.
+func RandomCurrency() string {
+	curr := []string{"USD", "EUR", "GBP", "CAD", "CNY", "AUD", "MXN"}
+	l := len(curr)
+
+	return curr[rand.Intn(l)]
+}
