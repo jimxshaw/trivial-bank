a724e49e80375b2efe12df7ca80153cceba12561
Jim Shaw <jimxshaw@gmail.com>
2023-08-21 11:27:50 -0500

    Add some TransferTx implementation.

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -20,6 +20,60 @@ func NewStore(db *sql.DB) *Store {
 	}
 }
 
+// TransferTxParams has parameters for a transfer transaction.
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResult is the result struct for a transfer transaction.
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx executes a money transfer from one account to another.
+// It creates a transfer record, entry records and update accounts balances
+// in a single db transaction.
+func (s *Store) TransferTx(ctx context.Context, params TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := s.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(params))
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: params.FromAccountID,
+			Amount:    params.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: params.ToAccountID,
+			Amount:    params.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: Update accounts balances for transfer transactions.
+
+		return nil
+	})
+
+	return result, err
+}
+
 // execTx executes the input function within a db transaction.
 func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := s.db.BeginTx(ctx, nil)
@@ -28,7 +82,7 @@ func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	}
 
 	// Get the queries from the transaction.
-	q := New(tx)
+	q := s.Queries.WithTx(tx)
 
 	err = fn(q)
 	if err != nil {
