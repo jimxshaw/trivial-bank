9bfcc6b262ad7a8503851d2e9acbb0bdf5bbcbd2
Jim Shaw <jimxshaw@gmail.com>
2023-09-28 16:43:06 -0500

    Add update account tests.

--- api/account_test.go
+++ api/account_test.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	mockdb "github.com/jimxshaw/trivial-bank/db/mocks"
 	db "github.com/jimxshaw/trivial-bank/db/sqlc"
@@ -42,6 +44,10 @@ func TestAccountAPI(t *testing.T) {
 
 	callList := func(m *mockdb.MockStore, params db.ListAccountsParams) *gomock.Call {
 		return m.EXPECT().ListAccounts(gomock.Any(), params)
+	}
+
+	callUpdate := func(m *mockdb.MockStore, params db.UpdateAccountParams) *gomock.Call {
+		return m.EXPECT().UpdateAccount(gomock.Any(), params)
 	}
 
 	callDelete := func(m *mockdb.MockStore, accountID int64) *gomock.Call {
@@ -153,7 +159,69 @@ func TestAccountAPI(t *testing.T) {
 
 	// TODO: Create Account.
 
-	// TODO: Update Account.
+	// Update Account.
+	t.Run("update account", func(t *testing.T) {
+		accountToUpdate := db.Account{
+			ID:        0,
+			Owner:     "Darth Vader",
+			Balance:   0,
+			Currency:  "USD",
+			CreatedAt: time.Date(1977, 5, 4, 0, 0, 0, 0, time.Local),
+		}
+
+		url := fmt.Sprintf("/accounts/%d", accountToUpdate.ID)
+		method := http.MethodPut
+
+		jsonStr := []byte(`{"owner":"Han Solo"}`)
+
+		params := db.UpdateAccountParams{
+			Owner: "Han Solo",
+		}
+
+		t.Run("happy path", func(t *testing.T) {
+			finish, m := newStoreMock(t)
+			defer finish()
+
+			accountToUpdate.Owner = "Han Solo"
+
+			callUpdate(m, params).
+				Times(1).
+				Return(accountToUpdate, nil)
+
+			server := newServerMock(m)
+			recorder := httptest.NewRecorder()
+
+			request, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
+			require.NoError(t, err)
+
+			request.Header.Set("Content-Type", "application/json")
+
+			server.router.ServeHTTP(recorder, request)
+
+			require.Equal(t, http.StatusOK, recorder.Code)
+		})
+
+		t.Run("some error happened", func(t *testing.T) {
+			finish, m := newStoreMock(t)
+			defer finish()
+
+			callUpdate(m, params).
+				Times(1).
+				Return(db.Account{}, errors.New("some error"))
+
+			server := newServerMock(m)
+			recorder := httptest.NewRecorder()
+
+			request, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
+			require.NoError(t, err)
+
+			request.Header.Set("Content-Type", "application/json")
+
+			server.router.ServeHTTP(recorder, request)
+
+			require.Equal(t, http.StatusInternalServerError, recorder.Code)
+		})
+	})
 
 	// Delete Account.
 	t.Run("delete account", func(t *testing.T) {
