7c1d659463568f1a06db0952ab331065d3e850b6
Jim Shaw <jimxshaw@gmail.com>
2023-09-28 14:36:05 -0500

    Add tests for list accounts.

--- api/account_test.go
+++ api/account_test.go
@@ -30,12 +30,78 @@ func newStoreMock(t *testing.T) (func(), *mockdb.MockStore) {
 func TestAccountAPI(t *testing.T) {
 	account := randomAccount()
 
+	accounts := []db.Account{
+		randomAccount(),
+		randomAccount(),
+	}
+
 	// Stubs.
 	callGet := func(m *mockdb.MockStore, accountID int64) *gomock.Call {
 		return m.EXPECT().GetAccount(gomock.Any(), accountID)
 	}
 
-	// TODO: List Accounts.
+	callList := func(m *mockdb.MockStore, params db.ListAccountsParams) *gomock.Call {
+		return m.EXPECT().ListAccounts(gomock.Any(), params)
+	}
+
+	// List Accounts.
+	t.Run("list accounts", func(t *testing.T) {
+		url := "/accounts"
+		method := http.MethodGet
+
+		params := db.ListAccountsParams{
+			Limit:  5,
+			Offset: 0,
+		}
+
+		t.Run("happy path", func(t *testing.T) {
+			finish, m := newStoreMock(t)
+			defer finish()
+
+			callList(m, params).
+				Times(1).
+				Return(accounts, nil)
+
+			server := newServerMock(m)
+			recorder := httptest.NewRecorder()
+
+			request, err := http.NewRequest(method, url, nil)
+			require.NoError(t, err)
+
+			query := request.URL.Query()
+			query.Add("page_id", "1")
+			query.Add("page_size", "5")
+			request.URL.RawQuery = query.Encode()
+
+			server.router.ServeHTTP(recorder, request)
+
+			require.Equal(t, http.StatusOK, recorder.Code)
+		})
+
+		t.Run("some error happened", func(t *testing.T) {
+			finish, m := newStoreMock(t)
+			defer finish()
+
+			callList(m, params).
+				Times(1).
+				Return([]db.Account{}, errors.New("some error"))
+
+			server := newServerMock(m)
+			recorder := httptest.NewRecorder()
+
+			request, err := http.NewRequest(method, url, nil)
+			require.NoError(t, err)
+
+			query := request.URL.Query()
+			query.Add("page_id", "1")
+			query.Add("page_size", "5")
+			request.URL.RawQuery = query.Encode()
+
+			server.router.ServeHTTP(recorder, request)
+
+			require.Equal(t, http.StatusInternalServerError, recorder.Code)
+		})
+	})
 
 	// Get Account.
 	t.Run("get account", func(t *testing.T) {
