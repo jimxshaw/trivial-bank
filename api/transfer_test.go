b824adc0c462654fd627b64994cec0498ab29a25
Jim Shaw <jimxshaw@gmail.com>
2023-09-30 18:00:01 -0500

    Update table tests for api transfer.

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -3,15 +3,72 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	mockdb "github.com/jimxshaw/trivial-bank/db/mocks"
 	db "github.com/jimxshaw/trivial-bank/db/sqlc"
 	"github.com/jimxshaw/trivial-bank/util"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func TestTransferAPI(t *testing.T) {
+	transfer := randomTransfer()
+
+	// Stubs.
+	callGet := func(m *mockdb.MockStore, transferID int64) *gomock.Call {
+		return m.EXPECT().GetTransfer(gomock.Any(), transferID)
+	}
+
+	// Table Testing
+	// Get Transfer test cases.
+	testCasesGetTransfer := []struct {
+		name          string
+		transferID    int64
+		stubs         func(m *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:       "happy path",
+			transferID: transfer.ID,
+			stubs: func(m *mockdb.MockStore) {
+				callGet(m, transfer.ID).
+					Times(1).
+					Return(transfer, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchTransfer(t, recorder.Body, transfer)
+			},
+		},
+	}
+
+	// Get Entry run test cases.
+	for i := range testCasesGetTransfer {
+		tc := testCasesGetTransfer[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			finish, m := newStoreMock(t)
+			defer finish()
+
+			tc.stubs(m)
+
+			server := newServerMock(m)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/transfers/%d", tc.transferID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+		})
+	}
 
 }
 
